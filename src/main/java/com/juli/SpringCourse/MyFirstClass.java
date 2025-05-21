0b82225074c885169942007910507088362bfdd6
JuliRulquinha <juli-felix@outlook.com>
2025-05-21 16:39:29 -0300

    First commit

---
+++ src/main/java/com/juli/SpringCourse/MyFirstClass.java
@@ -0,0 +1,16 @@
+package com.juli.SpringCourse;
+
+
+public class MyFirstClass {
+
+    private String myVar;
+
+    public MyFirstClass(String myVar) {
+        this.myVar = myVar;
+    }
+
+
+    public String sayHello(){
+        return "Hello from MyFirstClass ==> myVar = " + myVar;
+    }
+}
---
+++ src/test/java/com/juli/SpringCourse/SpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juli.SpringCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
